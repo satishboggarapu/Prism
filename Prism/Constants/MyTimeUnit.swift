08add7e78b74415e7ecb91d1e3f009dd42688c0a
Shiv Shah <shivs1995.ss@gmail.com>
2018-03-07 21:23:01 -0500

    MyTimeUnit file added

---
+++ Prism/Constants/MyTimeUnit.swift
@@ -0,0 +1,18 @@
+//
+//  MyTimeUnit.swift
+//  Prism
+//
+//  Created by Shiv Shah on 3/6/18.
+//  Copyright © 2018 Satish Boggarapu. All rights reserved.
+//
+
+import Foundation
+
+public class MyTimeUnit {
+
+        public static let SECONDS_UNIT = 60
+        public static let MINUTES_UNIT = 60
+        public static let HOURS_UNIT = 24
+        public static let DAYS_UNIT = 24 * 7
+        public static let YEARS_UNIT = 365
+}
