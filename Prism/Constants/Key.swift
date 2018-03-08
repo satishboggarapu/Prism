513154b95c255cdd738cb5b88cc2d23133a12860
Shiv Shah <shivs1995.ss@gmail.com>
2018-03-07 20:19:09 -0500

    added Key file

---
+++ Prism/Constants/Key.swift
@@ -0,0 +1,47 @@
+//
+//  Key.swift
+//  Prism
+//
+//  Created by Shiv Shah on 3/6/18.
+//  Copyright © 2018 Satish Boggarapu. All rights reserved.
+//
+
+import Foundation
+
+
+public class Key {
+    
+    // DO NOT CHANGE ANY OF THESE //
+    // THESE ARE LINKED TO MANY OTHER THINGS //
+    
+    public static let STORAGE_POST_IMAGES_REF = "POST_IMAGES";
+    public static let STORAGE_USER_PROFILE_IMAGE_REF = "USER_PROFILES";
+    public static let STORAGE_DEFALT_PROFILE_PICS_REF = "DEFAULT_PROFILES";
+    
+    public static let DB_REF_USER_PROFILES = "USERS";
+    public static let DB_REF_USER_UPLOADS = "USER_UPLOADS";
+    public static let DB_REF_USER_LIKES = "USER_LIKES";
+    public static let DB_REF_USER_REPOSTS = "USER_REPOSTS";
+    public static let DB_REF_USER_FOLLOWERS = "USER_FOLLOWERS";
+    public static let DB_REF_USER_FOLLOWINGS = "USER_FOLLOWINGS";
+    public static let USER_PROFILE_FULL_NAME = "fullname";
+    public static let USER_PROFILE_USERNAME = "username";
+    public static let USER_PROFILE_PIC = "profilepic";
+    
+    
+    public static let DB_REF_ALL_POSTS = "ALL_POSTS";
+    public static let DB_REF_POST_LIKED_USERS = "LIKED_USERS";
+    public static let DB_REF_POST_REPOSTED_USERS = "REPOSTED_USERS";
+    public static let POST_USERNAME = "username";
+    public static let POST_UID = "uid";
+    public static let POST_IMAGE_URI = "image";
+    public static let POST_DESC = "caption";
+    public static let POST_TIMESTAMP = "timestamp";
+    public static let POST_LIKES = "likes";
+    public static let POST_ID = "postId";
+    
+    public static let DB_REF_ACCOUNTS = "ACCOUNTS";
+    
+    
+    
+}
